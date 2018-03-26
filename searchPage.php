bab11a61ce3d790bb2b71c7e3ecfc7b5c33243e6
Soohyun Kim <shkim7534@gmail.com>
2018-03-25 17:31:43 -0700

    started search

---
+++ searchPage.php
@@ -0,0 +1,30 @@
+<p>If you wish to reset the table press on the reset button. If this is the first time you're running this page, you MUST use reset</p>
+<p>Search:</p>
+<form method="POST" action="searchPage.php">
+    <p> <input type=""
+        <input type="text" name="searchText" size="100">
+        <!--define two variables to pass the value-->
+        <input type="submit" value="search" name="searchSubmit"></p>
+</form>
+
+
+<?php
+include_once 'connection.php';
+if ($db_conn) {
+    // hardcoded to get all for now, modify to use LIKE
+    $result=  executePlainSQL("SELECT RECIPETITLE FROM RECIPE");
+    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+        echo "<div class='result'>";
+        if (array_key_exists("RECIPETITLE", $row)) {
+            echo "<p>" . $row["RECIPETITLE"] . "</p>";       
+        } else {
+            echo "<p>There are no recipes that match your search.</p>";
+        }
+    }
+    OCILogoff($db_conn);
+} else {
+    echo "cannot connect";
+    $e = OCI_Error(); // For OCILogon errors pass no handle
+    echo htmlentities($e['message']);
+}
+?>
