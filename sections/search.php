8f35ab9ceaa0fb8d9488ab6adf43abfacd1a560d
helenkuang <helen.kuang1@gmail.com>
2018-03-27 21:27:14 -0700

    Fixed search results to link to recipes

--- sections/search.php
+++ sections/search.php
@@ -42,21 +42,21 @@ require "header.php";
             switch ($searchBy) {
                 //searches are NOT case sensitive
                 case "title":
-                    $query = "SELECT RECIPETITLE FROM RECIPE WHERE UPPER(RECIPETITLE) LIKE UPPER('%" . $searchText . "%')";
+                    $query = "SELECT RECIPETITLE, RID FROM RECIPE WHERE UPPER(RECIPETITLE) LIKE UPPER('%" . $searchText . "%')";
                     break;
                 case "tags":
-                    $query = "SELECT DISTINCT RECIPETITLE FROM RECIPE r, SEARCHABLEBY s WHERE s.RID = r.RID AND UPPER(s.TAGNAME) LIKE UPPER('%" . $searchText . "%')";
+                    $query = "SELECT DISTINCT RECIPETITLE, RID FROM RECIPE r, SEARCHABLEBY s WHERE s.RID = r.RID AND UPPER(s.TAGNAME) LIKE UPPER('%" . $searchText . "%')";
                     break;
                 case "cuisine":
-                    $query = "SELECT RECIPETITLE FROM RECIPE WHERE UPPER(CUISINE) LIKE UPPER('%" . $searchText . "%')";
+                    $query = "SELECT RECIPETITLE, RID FROM RECIPE WHERE UPPER(CUISINE) LIKE UPPER('%" . $searchText . "%')";
                     break;
             }
             $result=  executePlainSQL($query);
             while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
                 echo "<div class='result'>";
-                if (array_key_exists("RECIPETITLE", $row)) {
-                    //TODO: Hyperlink to recipe page based on the search
-                    echo "<p><a href=\"\">" . $row["RECIPETITLE"] . "</a></p>";
+                if (array_key_exists("RECIPETITLE", $row) && array_key_exists("RID", $row)) {
+                    $rid = $row["RID"];
+                    echo "<p><a href='recipe.php?rid=".$rid."'>" . $row["RECIPETITLE"] . "</a></p>";
                 } else {
                     echo "<p>There are no recipes that match your search.</p>";
                 }
