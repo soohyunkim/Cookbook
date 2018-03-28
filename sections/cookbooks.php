007806ad2fcb488e42e5ea9196f0774b58115870
Soohyun Kim <shkim7534@gmail.com>
2018-03-27 21:23:05 -0700

    cookbook listing page works now

--- sections/cookbooks.php
+++ sections/cookbooks.php
@@ -19,22 +19,24 @@ require "header.php";
     </div>
     <!-- Submit Button -->
     <div class="cookbook-create-section">
-        <button type="submit" onClick="submitCookbookForm()" name="uploadRecipe">Create Cookbook</button>
+        <button type="submit" onClick="submitCookbookForm()" name="uploadCookbook">Create Cookbook</button>
     </div>
 </form>
-<p>Manage organized lists of recipes in your own custom cookbooks.</p>
 <?php
 
 include_once '../connection.php';
 
 // Connect Oracle...
 if ($db_conn) {
+
+    $userEmail = $_COOKIE['userEmail'];
+
     if (array_key_exists('cookbookSubmit', $_POST)) {
         $cookbookInfo = array(
             ":cookbookTitle" => $_POST['cookbookTitle'],
             ":description" => $_POST['cookbookDescription'],
             ":cid" => uniqid(),
-            ":email" => "emily604@sample.com" //TODO: change this to grab the current user's email
+            ":email" => $userEmail
         );
         $alltuples = array(
             $cookbookInfo
@@ -44,12 +46,26 @@ if ($db_conn) {
         OCICommit($db_conn);
     }
 
-    //Commit to save changes...
-    OCILogoff($db_conn);
+    if ($userEmail != null) {
+      $query = "SELECT COOKBOOKTITLE, DESCRIPTION, CID FROM MANAGEDCOOKBOOK WHERE EMAIL = '" . $userEmail . "'";
+      $result=  executePlainSQL($query);
+      echo "<p>View my cookbooks here: </p>";
+      while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+          echo "<div class='result'>";
+          if (array_key_exists("COOKBOOKTITLE", $row) && array_key_exists("CID", $row)) {
+              $cid = $row["CID"];
+              $title = $row["COOKBOOKTITLE"];
+              echo "<a href='cookbookrecipespage.php?cid=" . $cid . "'>" . $title . "</a>";
+          } else {
+              echo "<p>You have no cookbooks.</p>";
+          }
+      }
+      OCILogoff($db_conn);
+  }
 } else {
-    echo "cannot connect";
-    $e = OCI_Error(); // For OCILogon errors pass no handle
-    echo htmlentities($e['message']);
+  echo "cannot connect";
+  $e = OCI_Error(); // For OCILogon errors pass no handle
+  echo htmlentities($e['message']);
 }
 
 ?>
