c89041c354c560ed96660ebaa0de16dda15dd224
Jieun Lee <jieun-lee@users.noreply.github.com>
2018-03-27 20:10:30 -0700

    Add Logout Button

--- sections/header.php
+++ sections/header.php
@@ -20,22 +20,25 @@
                 <!-- Cookbook Tab Menu -->
                 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 cookbook-tab-menu">
                     <div class="list-group">
-                    <a href="search.php" id="menu-search" class="list-group-item text-center">
-                        <h4 class="glyphicon glyphicon-search"></h4>
-                        <br/>Search
-                    </a>
-                    <a href="cookbooks.php" id="menu-cookbooks" class="list-group-item text-center">
-                        <h4 class="glyphicon glyphicon-book"></h4>
-                        <br/>Cookbooks
-                    </a>
-                    <a href="bookmarks.php" id="menu-bookmarks" class="list-group-item text-center">
-                        <h4 class="glyphicon glyphicon-bookmark"></h4>
-                        <br/>Bookmarks
-                    </a>
-                    <a href="create.php" id="menu-create" class="list-group-item text-center">
-                        <h4 class="glyphicon glyphicon-pencil"></h4>
-                        <br/>Create
-                    </a>
+                        <a href="search.php" id="menu-search" class="list-group-item text-center">
+                            <h4 class="glyphicon glyphicon-search"></h4>
+                            <br/>Search
+                        </a>
+                        <a href="cookbooks.php" id="menu-cookbooks" class="list-group-item text-center">
+                            <h4 class="glyphicon glyphicon-book"></h4>
+                            <br/>Cookbooks
+                        </a>
+                        <a href="bookmarks.php" id="menu-bookmarks" class="list-group-item text-center">
+                            <h4 class="glyphicon glyphicon-bookmark"></h4>
+                            <br/>Bookmarks
+                        </a>
+                        <a href="create.php" id="menu-create" class="list-group-item text-center">
+                            <h4 class="glyphicon glyphicon-pencil"></h4>
+                            <br/>Create
+                        </a>
+                        <form id="logout-form" action="logout.php">
+                            <button type="submit" class="logout-button" name="logout">Logout</button>
+                        </form>
                     </div>
                 </div>
 
